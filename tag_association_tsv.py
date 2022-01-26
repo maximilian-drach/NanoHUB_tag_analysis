1eafa2463d515dd83e5d766ee30faf1949ea7a49
maximilian-drach <80358780+maximilian-drach@users.noreply.github.com>
2022-01-26 18:40:41 -0500

    changes to the script

--- tag_association_tsv.py
+++ tag_association_tsv.py
@@ -5,8 +5,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='A script that returns a tsv file of the frequency of all the tags associated with a tag')
-    parser.add_argument('-tdf', '-tags_df', type=str, default='tags.tsv', dest='tagsDF_path', help='Specify the path to the tags.tsv adn tags_obj.tsv file (the file with all the tags number and names)')
-    #parser.add_argument('-todf', '-tags_obj', type=str, default='tags_obj.tsv', dest='tags_objDF', help='Specify the tags_obj.tsv file (the file with all courses, resources, etc)')
+    parser.add_argument('-tdf', '-tags_df', type=str, dest='tagsDF_path', help='Specify the path to the tags.tsv adn tags_obj.tsv file (the file with all the tags number and names)')
     parser.add_argument('-t', '-tag_name', dest='tag', nargs='?', type=str,  help='Put this at the end of the script and type the tag name.')
     parser.add_argument('-o', '-o', dest='output', type=str, help='Type the outputs path for the tsv file')
     results = parser.parse_args()
